4b7a1de82ec514e6c170f42acbdf7bf009029b8b
armellesuperlife <armelle.supervie@cri.org>
2015-11-11 17:17:01 +0100

    19commit

--- exercises/019/solution.py
+++ exercises/019/solution.py
@@ -1,8 +1,6 @@
 import sys
 x = len(sys.argv)
-a = sys.argv[2]
-b = sys.argv[3]
-if x > 2:
-    print int(a) + int(b)
+if x > 1:
+    print(int(sys.argv[1]) + int(sys.argv[2]))
 else:
     print("usage: python3 solution.py OP1 OP2")
