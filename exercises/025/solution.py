14f14c082c8a378540fbbb34dd99471116996efe
armellesuperlife <armelle.supervie@cri.org>
2015-11-20 15:23:54 +0100

    25commit

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -1,3 +1,4 @@
 from time import gmtime, strftime
-print("Today is " + strftime("%Y") + "-" + strftime("%m") + 
-"-" + strftime("%d") + " and it is "+ strftime("%X"))
+print(
+    "Today is " + strftime("%Y") + "-" + strftime("%m")
+    + "-" + strftime("%d") + " and it is "+ strftime("%X"))
