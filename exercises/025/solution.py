c7aad97d8817161f7651a9bfd21b83094e283c90
armellesuperlife <armelle.supervie@cri.org>
2015-11-20 16:19:03 +0100

    25commit

--- exercises/025/solution.py
+++ exercises/025/solution.py
@@ -1,10 +1,10 @@
 from time import gmtime, strftime
-a == "Today is "
-b == strftime("%Y")
-c == "-"
-d == strftime("%m")
-e == "-"
-f == strftime("%d")
-g == " and it is "
-h == strftime("%X")
+a = "Today is "
+b = strftime("%Y")
+c = "-"
+d = strftime("%m")
+e = "-"
+f = strftime("%d")
+g = " and it is "
+h = strftime("%X")
 print(a + b + c + d + e + f + g + h)
